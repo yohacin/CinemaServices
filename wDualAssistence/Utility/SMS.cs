8e271e9b1980649a5dcf25181f7bd3360b6681cd
yohacin <yohacin@gmail.com>
2025-01-18 00:05:15 -0400

    final build ]Ok

--- wDualAssistence/Utility/SMS.cs
+++ wDualAssistence/Utility/SMS.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Linq;
+
 using System.Text;
 
-using System.Threading.Tasks;
+
 
 using Newtonsoft.Json.Linq;
 
@@ -15,16 +15,17 @@ using ENO = Entities.Notificador;
 using BNO = Business.Notificador;
 using System.Net.Http.Json;
 
+
 namespace wDualAssistence.Utility
 {
-    public class SMS
-    {
+    public class SMS{
+
         private string destino;
         private string origen;
         private string texto;
 
-        private string username = "DUALBIZ";
-        private string password = "dualbiz123.";
+        private string username = "";
+        private string password = "";
         public static HttpClient client;
 
         public string Destino
@@ -42,24 +43,17 @@ namespace wDualAssistence.Utility
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
+
         public async Task<string> enviarSMSGet()
         {
             try
             {
-                string url = "sms/1/text/query?" +
-                    "username=" + username +
-                    "&macpnPassw=" + password +
-                    "&to=" + Destino +
-                    "&from=" + Origen +
-                    "&text=" + Texto;
-                HttpResponseMessage response = await client.GetAsync(url);
-                //Log.writeErrorLog("Se ha enviado el mensaje de texto con exito!");
+              
                 return "";
             }
             catch (Exception ex)
             {
-                //Log.writeErrorLog("Error: " + ex.ToString());
-                //return ("Error:" + ex.Message);
+              
                 return "Error: no se ha podido enviar el mensaje de texto al numero " + destino.ToString();
             }
         }
@@ -68,42 +62,14 @@ namespace wDualAssistence.Utility
         {
             try
             {
-                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(username + ":" + password);
-                string encode = System.Convert.ToBase64String(bytes);
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encode);
-
-                var cuerpo = new
-                {
-                    to = Destino,
-                    from = Origen,
-                    text = Texto
-                };
-
-                HttpResponseMessage response = await client.PostAsJsonAsync("sms/2/text/single", cuerpo);
-                //HttpResponseMessage response = client.PostAsJsonAsync("sms/2/text/single", cuerpo).Result;
-                JObject jObject = JObject.Parse(await response.Content.ReadAsStringAsync());
-                //JObject jObject = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-                JToken messages = jObject["messages"];
-                JArray mensajes = (JArray)messages;
-                JToken mensaje = mensajes[0];
-                JToken status = mensaje["status"];
-                string resultado = status.Value<string>("name") + ", " + status.Value<string>("description");
-                response.EnsureSuccessStatusCode();
-                if (response.IsSuccessStatusCode)
-                {
-                    //Log.writeErrorLog("Se ha enviado el mensaje de texto con exito al numero " + destino);
-                    //Log.writeErrorLog("Datos extras: " + resultado);
+              
                     return "";
-                }
-                else
-                {
-                    throw new Exception("Fallo al enviar el SMS. Motivo: " + resultado);
-                }
+              
+              
             }
             catch (Exception ex)
             {
-                //Log.writeErrorLog("Error: " + ex.ToString());
-                //return "Error: " + ex.Message;
+              
                 return "Error: no se ha podido enviar el mensaje de texto al numero " + destino.ToString();
             }
         }
@@ -112,38 +78,8 @@ namespace wDualAssistence.Utility
         {
             try
             {
-                bool result = false;
-                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(username + ":" + password);
-                string encode = System.Convert.ToBase64String(bytes);
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encode);
-
-                var cuerpo = new
-                {
-                    to = "591" + sms_prueba,
-                    from = "DualAssistance",
-                    text = content
-                };
-
-                HttpResponseMessage response = await client.PostAsJsonAsync("sms/2/text/single", cuerpo);
-                //HttpResponseMessage response = client.PostAsJsonAsync("sms/2/text/single", cuerpo).Result;
-                JObject jObject = JObject.Parse(await response.Content.ReadAsStringAsync());
-                //JObject jObject = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-                JToken messages = jObject["messages"];
-                JArray mensajes = (JArray)messages;
-                JToken mensaje = mensajes[0];
-                JToken status = mensaje["status"];
-                string resultado = status.Value<string>("name") + ", " + status.Value<string>("description");
-                response.EnsureSuccessStatusCode();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    result = true;
-                }
-                else
-                {
-                    result = false;
-                }
-                return result;
+              
+                return true;
             }
             catch (Exception ex)
             {
@@ -155,35 +91,9 @@ namespace wDualAssistence.Utility
         {
             try
             {
-                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(username + ":" + password);
-                string encode = System.Convert.ToBase64String(bytes);
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encode);
-
-                var cuerpo = new
-                {
-                    to = "591" + sms_prueba,
-                    from = "Factura",
-                    text = content
-                };
-
-                HttpResponseMessage response = await client.PostAsJsonAsync("sms/2/text/single", cuerpo);
-                //HttpResponseMessage response = client.PostAsJsonAsync("sms/2/text/single", cuerpo).Result;
-                JObject jObject = JObject.Parse(await response.Content.ReadAsStringAsync());
-                //JObject jObject = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-                JToken messages = jObject["messages"];
-                JArray mensajes = (JArray)messages;
-                JToken mensaje = mensajes[0];
-                JToken status = mensaje["status"];
-                string resultado = status.Value<string>("name") + ", " + status.Value<string>("description");
-                response.EnsureSuccessStatusCode();
-                if (response.IsSuccessStatusCode)
-                {
+                
                     return "";
-                }
-                else
-                {
-                    throw new Exception("Fallo al enviar el SMS. Motivo: " + resultado);
-                }
+                
             }
             catch (Exception ex)
             {
@@ -193,34 +103,8 @@ namespace wDualAssistence.Utility
             }
         }
 
-        //public async Task<string> enviarSMSAmazonAsync()
-        //    {
-        //        try
-        //        {
-        //            AmazonSimpleNotificationServiceClient sns = new AmazonSimpleNotificationServiceClient("AKIAI64EBMAGZ5BNIBXA", "U88Oe8EAjjWLKAGew4Kct+AbBTe3iaIu4Rso58he", Amazon.RegionEndpoint.USEast1);
-        //            Dictionary<String, MessageAttributeValue> dicAttr = new Dictionary<String, MessageAttributeValue>();
-        //            MessageAttributeValue value = new MessageAttributeValue();
-        //            value.StringValue = Origen;
-        //            value.DataType = "String";
-        //            dicAttr.Add("AWS.SNS.SMS.SenderID", value);
 
-        //            PublishResponse response = await sns.PublishAsync(new PublishRequest() { PhoneNumber = Destino, Message = Texto, MessageAttributes = dicAttr });
-        //            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
-        //            {
-        //                //Log.writeErrorLog("Se ha enviado el mensaje de texto con exito al numero " + destino);
-        //                return "";
-        //            }
-        //            else
-        //            {
-        //                throw new Exception("Fallo al enviar el SMS. Motivo: " + response.MessageId);
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            //Log.writeErrorLog("Error: " + ex.ToString());
-        //            return "Error: no se ha podido enviar el mensaje de texto al numero " + destino.ToString();
-        //        }
-
-        //    }
     }
+
 }
+    
